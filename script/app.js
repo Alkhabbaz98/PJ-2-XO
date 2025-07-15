40242d9e9c5a67f3ba849f788f559a13b35caf65
unknown <mohammed98ali.ma@gmail.com>
2025-07-15 13:35:00 +0300

    Tie problem finished. Buttons next

--- script/app.js
+++ script/app.js
@@ -15,9 +15,10 @@ const winningCombos = [
 /*---------------------------- Variables (state) ----------------------------*/
 // Use board string same as the cells in duck game
 let board
-let turn 
+let Playerturn 
 let winner
 let tie 
+let CPUturn
 // let CPUmark
 // let playerVplayer
 // let cpu = [0, 1 , 2 , 3 , 4, 5 , 6, 7, 8]
@@ -25,7 +26,7 @@ let tie
 const squareElems = document.querySelectorAll('.sqr')
 const messageEl = document.querySelector('#message')
 const SetX = document.querySelector('.SetX')
-const SetO = document.querySelector('.SetO')
+// const SetO = document.querySelector('.SetO')
 const HowToPlayBtn = document.querySelector('#HowToPlayBtn')
 const HowToPlay = document.querySelector('.HowToPlay')
 const closed = document.querySelector('.closed')
@@ -51,17 +52,17 @@ for (i = 0; i<board.length ; i++){
 }
 
 function updateMessage(squareIndex){
-    if (winner === false && tie === false && turn === ''){ 
+    if (winner === false && tie === false && Playerturn === ''){ 
         return messageEl.textContent = 'Choose X or O to Play'
-    } else if (winner === false && tie === false && turn === 'O') {
+    } else if (winner === false && tie === false && Playerturn === 'O') {
        return messageEl.textContent = 'Now O turn'
-    } else if (winner === false && tie === false && turn === 'X'){
+    } else if (winner === false && tie === false && Playerturn === 'X'){
         return messageEl.textContent = 'Now X turn'
     }
     else if (winner === false && tie === true){
         messageEl.textContent = 'It is a TIE'
     } 
-    else  if (winner === true && turn === 'X' && board[squareIndex] != ''){
+    else  if (winner === true && Playerturn === 'X' && board[squareIndex] != ''){
        return messageEl.textContent = 'O won'
     } else {
         messageEl.textContent = 'X won'
@@ -72,17 +73,17 @@ function updateMessage(squareIndex){
 // console.log(updateMessage())
 
 function placePiece(index){
-    if(!turn) {
+    if(!Playerturn) {
         return;
     } else {
-        board[index] = turn
+        board[index] = Playerturn
     }
     switchPlayerTurn()
 
 }
 
 function handleClick(event){
-        if(!turn) return;
+        if(!Playerturn) return;
     // squareElems.forEach(function(OnesqrElm){
     //     const squareIndex = squareElems[OnesqrElm].event.target.id
     // })
@@ -92,12 +93,22 @@ function handleClick(event){
 }
 
         placePiece(squareIndex)
+        console.log('1', {winner})
+        // if(winner) return
+        checkForTie()
+        checkForWinner()
         CPUfun()
+        checkForTie()
+        checkForWinner()
+        console.log('2', {winner})
         // console.log(squareIndex)
         // switchPlayerTurn()
-        checkForWinner()
-        checkForTie()
+        
+        console.log('3', {winner})
+        
+        console.log('4', {winner})
         render()
+        console.log('5', {winner})
 
 }
 
@@ -120,12 +131,21 @@ function checkForWinner(){
     (board[2] !== '' && board[2] === board[5] && board[2] === board[8]) ||
     (board[0] !== '' && board[0] === board[4] && board[0] === board[8]) ||
     (board[2] !== '' && board[2] === board[4] && board[2] === board[6])){
+        console.log('winner should go true')
         winner = true
     }
     }   
 
 
 function checkForTie(){
+
+      const isFilled = board.every((tile)=>{
+        return tile !== ""
+    })
+    if (Playerturn === 'O' && winner === false && isFilled){
+        console.log("tie")
+        return
+    }
     if (winner === true){
         return 
     }
@@ -138,10 +158,10 @@ function switchPlayerTurn(){
     if (winner === true) {
         return 
     }
-    if (turn === 'X'){
-            turn = 'O'
+    if (Playerturn === 'X'){
+            Playerturn = 'O'
     } else {
-            turn = 'X'
+            Playerturn = 'X'
     }
     }
 
@@ -151,31 +171,31 @@ function render(){
     updateMessage()
 }
 
-function SetOfun(){
-    // playerVplayer = true 
-    if(turn) {
-        SetX.classList.add("hidden")
-        SetO.classList.add("hidden")
-        return ;
-    } else {
-        turn = 'O'
-        // messageEl.textContent = 'Now O turn'
-        SetX.classList.add("hidden")
-        SetO.classList.add("hidden")
-    }
-    messageEl.textContent = 'Now O turn'
-    // console.log(turn)
-}
+// function SetOfun(){
+//     // playerVplayer = true 
+//     if(Playerturn) {
+//         SetX.classList.add("hidden")
+//         SetO.classList.add("hidden")
+//         return ;
+//     } else {
+//         Playerturn = 'O'
+//         // messageEl.textContent = 'Now O turn'
+//         SetX.classList.add("hidden")
+//         SetO.classList.add("hidden")
+//     }
+//     messageEl.textContent = 'Now O turn'
+//     // console.log(turn)
+// }
 function SetXfun(){
 //    playerVplayer = true 
-    if(turn) {
+    if(Playerturn) {
         // messageEl.textContent = 'Now X turn'
-        SetO.classList.add("hidden")
+        // SetO.classList.add("hidden")
         SetX.classList.add("hidden")
         return;
     } else {
-        turn = 'X'
-        SetO.classList.add("hidden")
+        Playerturn = 'X'
+        // SetO.classList.add("hidden")
         SetX.classList.add("hidden")
     }
     // console.log(turn)
@@ -185,19 +205,36 @@ function SetXfun(){
 
 
 function CPUfun(){
+
+      const isFilled = board.every((tile)=>{
+        return tile !== ""
+    })
+    if (Playerturn === 'O' && winner === false && isFilled){
+        console.log("tie")
+        return
+    }
     console.log('CPU Fun is running')
     console.log(winner)
     // playerVplayer = false
     // if (playerVplayer === false){
-    if(turn === 'O' && winner === false){
+    if(Playerturn === 'O' && winner === false){
     let randomInd = Math.floor(Math.random() * 9)
     console.log(typeof board[randomInd])
     if (board[randomInd]) {
         return CPUfun()
     } else if (board[randomInd] === '' && winner === true){
         return
-    }
+    // } else if (!board.includes('') && winner === false){
+    //     checkForTie()
+    //     return
+
+  
+    } 
+    
+
+
     board[randomInd] = 'O'
+    // checkForTie()
     switchPlayerTurn()
    }
 }
@@ -209,13 +246,13 @@ function init(){
           '','','',
           '','','']
 
- turn = ''
+ Playerturn = ''
  winner = false
  tie = false
-//  let CPUmark = 'O'
- let playerVplayer 
+ CPUturn = ''
+  
 
- render()
+render()
 
 }
 
@@ -224,7 +261,7 @@ init();
 squareElems.forEach(function(OnesqrElm){
 OnesqrElm.addEventListener('click', handleClick)
 SetX.addEventListener('click',SetXfun)
-SetO.addEventListener('click',SetOfun)
+// SetO.addEventListener('click',SetOfun)
 })
 
 HowToPlayBtn.addEventListener('click', clickInstruction)
